4603c98d25e0a21038ccc407f7b0f3d8d3abb148
li <768353841@qq.com>
2022-12-09 01:18:03 +0800

    fuzi

--- src/layouts/MainLayoutSt.vue
+++ src/layouts/MainLayoutSt.vue
@@ -1,21 +1,20 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header v-model="drawerOpen">
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
         <q-toolbar-title>
-          stock lin
+          stock
         </q-toolbar-title>
         <div>Stock v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered :width="200" :breakpoint="1500">
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered :width="150" :breakpoint="1500">
       <q-list>
         <q-item-label header>
-          Essential Links
+          Essential
         </q-item-label>
-
         <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
@@ -27,31 +26,32 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, provide } from 'vue';
 import EssentialLink from 'components/EssentialLink.vue';
+
 
 const linksList = [
   {
     title: 'tab',
-    caption: 'quasar.dev',
+    caption: 'quasar',
     icon: 'school',
     link: 'http://localhost:9000/#/tab'
   },
   {
     title: 'vote',
-    caption: 'github.com/quasarframework',
+    caption: 'github',
     icon: 'code',
     link: 'http://localhost:9000/#/polls/5/vote/'
   },
   {
     title: 'mytest',
-    caption: 'chat.quasar.dev',
+    caption: 'chat',
     icon: 'chat',
     link: 'http://localhost:9000/#/mytest'
   },
   {
     title: 'stockYjbbEm',
-    caption: 'forum.quasar.dev',
+    caption: 'forum',
     icon: 'record_voice_over',
     link: 'http://localhost:9000/#/polls/stockYjbbEm/'
   },
@@ -69,7 +69,7 @@ const linksList = [
   },
   {
     title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
+    caption: 'Community',
     icon: 'favorite',
     link: 'https://awesome.quasar.dev'
   }
@@ -84,13 +84,20 @@ export default defineComponent({
 
   setup() {
     const leftDrawerOpen = ref(false)
+    const drawerOpen = ref(false)
+    function toggleLeftDrawer() {
+      leftDrawerOpen.value = !leftDrawerOpen.value
+    }
+    function toggleDrawer() {
+      drawerOpen.value = !drawerOpen.value
+    }
+    provide('toggleDrawer', toggleDrawer)
 
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      drawerOpen,
+      toggleLeftDrawer
     }
   }
 });
--- src/pages/PageZhangTing.vue
+++ src/pages/PageZhangTing.vue
@@ -1,21 +1,98 @@
 <template>
-  <!-- <KeepAlive> -->
-  <TableZhangTing></TableZhangTing>
-  <!-- <TableStock :data="data"></TableStock> -->
-  <!-- </KeepAlive> -->
+  <!-- xs=4 md中,sm=8所有方向小的padding -->
+  <div class="q-pa-xs">
+    <q-table title="Treats" :rows="rows" :columns="columns" row-key="name" dense
+      :rows-per-page-options="[50, 100, 200, 500, 1000, 0]" rows-per-page-label=" " flat bordered
+      class="my-sticky-header-table" :visible-columns="visibleColumns" :filter="filter">
+      <template v-slot:top-right>
+        <TableZhangTing :flatLable="flatLable = '当天涨停'" :nameList="onSubmitFather"></TableZhangTing>
+        <q-select v-model="visibleColumns" multiple outlined dense options-dense :display-value="$q.lang.table.columns"
+          emit-value map-options :options="columns" option-value="name" options-cover style="min-width: 150px" />
+
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+      <template v-slot:body-cell-0="props">
+        <q-td :props="props">
+          <a target="_blank" :href="'#/stockStandardk/' + props.value + '/code2/' + code2 + '/name2/' + name2">
+            {{ props.value }}
+          </a>
+        </q-td>
+      </template>
+    </q-table>
+    <!-- <div>我:{{ linksList }}</div> -->
+  </div>
 </template>
 
 <script>
 import TableZhangTing from 'components/TableZhangTing.vue';
-import { defineComponent } from 'vue';
 
-export default defineComponent({
+
+export default {
   name: 'PageZhangTing',
   components: { TableZhangTing },
-  // setup() {
-  //   // let data = reactive({ col: [], da: [], code2: [], name2: [], names: [] });
+  data() {
+    return {
+      rows: [],
+      columns: [],
+      code2: [],
+      name2: [],
+      visibleColumns: [],
+      filter: '',
+    };
+  },
+  // props: {
+  //   data: {
+  //     type: Object
+  //   },
+  // },
+  methods: {
+    onSubmitFather(val) {
+      // console.log(val.data.col.map(function (user) { return user.name; }))
+      this.rows = val.data.da
+      this.columns = val.data.col
+      this.code2 = val.data.code2
+      this.name2 = val.data.name2
+      this.visibleColumns = val.data.col.map(function (user) { return user.name; })
+    }
+  },
+  // setup() { 
   //   return {
-  //   };
+  //     filter: ref(''),
+  //   }
   // }
-});
+}
 </script>
+<style lang="sass">
+.my-sticky-header-table
+  /* height or max-height is important */
+  height: 33.85rem
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th
+    /* bg color is important for th; just specify one */
+    background-color: #c1f4cd
+
+  thead tr th
+    position: sticky
+    z-index: 1
+  thead tr:first-child th
+    top: 0
+
+  /* this is when the loading indicator appears */
+  &.q-table--loading thead tr:last-child th
+    /* height of all previous header rows */
+    top: 48px
+</style>
+<style type="text/css">
+a {
+  /* 去除默认的下划线 */
+  text-decoration: none;
+  /* 去除默认的颜色和点击后变化的颜色 */
+  color: #000;
+}
+</style>
